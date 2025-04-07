5684d18b9643c83344b624b1abdb9468be1e16fe
Pedro Jimenez <pedro.jimenez@aluno.senai.br>
2025-04-07 14:04:26 -0300

    Anotações main
    Adicionando main

---
+++ ATV/Trabalho.py
@@ -0,0 +1,10 @@
+# ============================
+# Código Funcional Completo
+# ============================
+
+print("=== Verificador de idade ===")
+idades = []
+
+for i in range(2):
+    idade = int(input("Digite um número: "))
+    idades.append(idade)
